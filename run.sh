d783e5d52848587637a34853e5d3645fa6e0eb04
lsiddd <lucassidpacheco@gmail.com>
2020-05-21 08:22:28 -0300

    run script with applicationType

--- run.sh
+++ run.sh
@@ -3,9 +3,13 @@
 dir=$(basename "$PWD")
 
 mkdir runs
-for i in $(seq 6 10)
+for k in $(seq 0 2)
 do
-  mkdir runs/simul$i
-  time ./waf --run "mswim --randomSeed=$i --algorithm=$dir" > runs/simul$i/${dir}_${i}.txt
-  mv *xml *txt *csv runs/simul$i
+	mkdir runs/type${k}
+	for i in $(seq 6 10)
+	do
+	  mkdir runs/type${k}/simul$i
+	  time ./waf --run "mswim --randomSeed=$i --algorithm=$dir --applicationType=$k" > runs/type${k}/simul$i/${dir}_${i}.txt
+	  mv *xml *txt *csv runs/type${k}/simul$i
+	done
 done
